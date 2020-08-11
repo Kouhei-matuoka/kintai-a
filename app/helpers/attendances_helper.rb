a0d3ddaca1d194f9ba9ec659de3bb0b23d9fbe2a
Kouhei-matuoka <kouhei19951112@gmail.com>
2020-08-11 05:11:36 +0000

    Finish add-user-attendances

---
+++ app/helpers/attendances_helper.rb
@@ -0,0 +1,20 @@
+module AttendancesHelper
+
+  def attendance_attend(attendance)
+    if Date.current == attendance.worked_on
+      return '出社' if attendance.started_at.nil?
+    end
+    false
+  end
+
+  def attendance_leave(attendance)
+    if Date.current == attendance.worked_on
+      return '退社' if attendance.started_at.present? && attendance.finished_at.nil?
+    end
+    false
+  end
+  
+  def working_times(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
+  end
+end
